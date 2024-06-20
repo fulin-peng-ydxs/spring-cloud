f3685e48102393335ca706f997c6c2df303cad30
pengfulin <1162855925@qq.com>
2024-06-20 15:11:15 +0800

    -gateway加入跨域处理

---
+++ cloud-gateway/src/main/java/gateway/conf/CorsConfig.java
@@ -0,0 +1,29 @@
+package gateway.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * 加入跨域配置
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        //配置跨域信息：这里的*不同于直接写在响应头里的*，服务会根据预请求自适应返回
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        //按需开启
+//        config.setAllowCredentials(true);
+        //配置允许跨域的资源路径
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
